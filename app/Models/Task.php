796c17bcc5f25cd0106ddef92060b7306cb100c3
adam <a2e56b85@gmail.com>
2025-12-07 20:29:10 +0800

    finally done

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Task extends Model
 {
     protected $guarded = [];
+
+    protected $appends = ['submission_display'];
+
+    protected $hidden = ['submission_base64'];
 
     // public function team()
     // {
@@ -21,5 +26,13 @@ class Task extends Model
     public function team()
     {
         return $this->belongsTo(Team::class);
+    }
+
+    protected function submissionDisplay(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => 
+                $attributes['submission_filename'] ?? $attributes['submission'] ?? null
+        );
     }
 }
